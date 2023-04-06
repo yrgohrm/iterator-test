571cf1a17a2ea1a6f1d6a94d35d968e51bbeb2cd
Hampus Ram <hampus.ram@educ.goteborg.se>
2023-04-06 13:21:56 +0200

    a solution

--- src/main/java/se/yrgo/it/FlattenedIterator.java
+++ src/main/java/se/yrgo/it/FlattenedIterator.java
@@ -3,26 +3,38 @@ package se.yrgo.it;
 import java.util.*;
 
 /**
- * Given a list of iterators this class acts as if all those iterators
- * where a single iterator. I.e. the iterators should be flattened out 
- * into one big iterator. The iterators should be iterated through
- * in the order of the list, from first to last.
+ * Given a list of iterators this class acts as if all those iterators where a single iterator. I.e.
+ * the iterators should be flattened out into one big iterator. The iterators should be iterated
+ * through in the order of the list, from first to last.
  * 
  */
 public class FlattenedIterator<T> implements Iterator<T> {
 
+    private final Iterator<Iterator<T>> iterators;
+    private Iterator<T> currentIterator;
+
     public FlattenedIterator(List<Iterator<T>> iterators) {
-        throw new UnsupportedOperationException("Unimplemented constructor");
+        this.iterators = List.copyOf(iterators).iterator();
+
+        nextIterator();
     }
 
     /**
-     * Returns true if any of the iterators has more elements. Returns
-     * false when the last iterator returns false.
+     * Returns true if any of the iterators has more elements. Returns false when the last iterator
+     * returns false.
      * 
      */
     @Override
     public boolean hasNext() {
-        throw new UnsupportedOperationException("Unimplemented method 'hasNext'");
+        while (currentIterator != null && !currentIterator.hasNext()) {
+            nextIterator();
+        }
+
+        if (currentIterator == null) {
+            return false;
+        }
+
+        return currentIterator.hasNext();
     }
 
     /**
@@ -31,6 +43,18 @@ public class FlattenedIterator<T> implements Iterator<T> {
      */
     @Override
     public T next() {
-        throw new UnsupportedOperationException("Unimplemented method 'next'");
-    }    
+        while (currentIterator != null && !currentIterator.hasNext()) {
+            nextIterator();
+        }
+
+        if (currentIterator == null) {
+            throw new NoSuchElementException();
+        }
+
+        return currentIterator.next();
+    }
+
+    private void nextIterator() {
+        currentIterator = iterators.hasNext() ? iterators.next() : null;
+    }
 }
